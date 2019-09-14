4439e62edd0bf8f6d6f129067dfe61cd888033f5
ryu.furu <fulufulu6278@gmail.com>
2019-09-14 05:21:03 +0000

    favoreites

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :favorites
+    has_many :likes, through: :favorites, source: :micropost
+    
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -26,8 +29,25 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
   
+  def favorite(other_micropost)
+    unless self == other_micropost
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+    end
+  end
+  
+  def unfavorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(other_micropost)
+    self.likes.include?(other_micropost)
+  end
+  
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+
 end
 
